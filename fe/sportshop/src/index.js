69531eeccbc05a43b4deafe5d4814eda2d0aca57
vanphan04 <vanphan44330003@gmail.com>
2025-06-03 15:39:51 +0700

    git3

--- fe/sportshop/src/index.js
+++ fe/sportshop/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import RouterCustom from './router';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>SportShop</h1>
+  <BrowserRouter>
+    <RouterCustom />
+  </BrowserRouter>
 );
